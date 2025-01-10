08f3f7830ebb7132b8239f5bf3ff8a09f37c022b
regr4 <60316183+regr4@users.noreply.github.com>
2025-01-10 11:51:02 +0100

    Add statement of central lemma

--- LeanTutorial/Chapter2.lean
+++ LeanTutorial/Chapter2.lean
@@ -1,5 +1,9 @@
-import Mathlib
+import Mathlib.RingTheory.Coprime.Basic
+import Mathlib.Algebra.Prime.Defs
+import Mathlib.Algebra.Polynomial.Basic
+import Init.Data.Int.Basic
+import Mathlib.Algebra.Divisibility.Basic
 
-#eval 1+1
-
--- code
+theorem primality (n : ℕ) (hn : n ≥ 2) (a : ℤ) (coprime : IsCoprime (↑ n) a)
+  : Prime n ↔ Polynomial.C (R := ℤ) (↑ n) ∣ ((X : Polynomial ℤ) + Polynomial.C a)^n - Polynomial.monomial n 1 + Polynomial.C a
+  := by sorry
