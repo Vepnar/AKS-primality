fa6634672bc4ea2c71e9fec700f5811ff91accf3
zuzapio <zpiotrow16@gmail.com>
2025-01-10 12:36:54 +0100

    Add binomial lemma

--- LeanTutorial/Chapter2.lean
+++ LeanTutorial/Chapter2.lean
@@ -7,3 +7,13 @@ import Mathlib.Algebra.Divisibility.Basic
 theorem primality (n : ℕ) (hn : n ≥ 2) (a : ℤ) (coprime : IsCoprime (↑ n) a)
   : Prime n ↔ Polynomial.C (R := ℤ) (↑ n) ∣ ((X : Polynomial ℤ) + Polynomial.C a)^n - Polynomial.monomial n 1 + Polynomial.C a
   := by sorry
+
+-- define the polynomial in z mod n, it might be easier
+
+-- (n choose k) = 0
+
+-- quotients, morphisms, ideal.quotient.blabla, ring to quotient
+
+theorem n_choose_k (n : ℕ ) (k : ℕ ) (hk1 : k < n) (hk2 : k > 0) (hn : Prime n)
+  : n.choose k % n = 0
+  := by sorry
