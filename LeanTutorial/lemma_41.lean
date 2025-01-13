030725895cdf21bda0a2fff3899f6418fa3a0e72
regr4 <60316183+regr4@users.noreply.github.com>
2025-01-13 19:23:37 +0100

    Define H, G, S from lemma 4.1

--- LeanTutorial/lemma_41.lean
+++ LeanTutorial/lemma_41.lean
@@ -4,7 +4,7 @@ open Polynomial
 noncomputable def ZtoZp (p : ℕ ) := Polynomial.map (Int.castRingHom (ZMod p))
 #check ZtoZp 5 (6*X^5)
 
-def extracth
+-- def extracth
 
 def bigF (p : ℕ ) (h : Polynomial (ZMod p))
 := AdjoinRoot h
@@ -13,7 +13,7 @@ def bigF (p : ℕ ) (h : Polynomial (ZMod p))
 -- haveI for inst
 -- let for functions
 -- have for proof of prop
-noncomputable instance (p : ℕ ) [Fact (Nat.Prime p)] (h : Polynomial (ZMod p)) [Fact (Irreducible h)] : Field (bigF p h) := by
+noncomputable instance (p : ℕ) [Fact (Nat.Prime p)] (h : Polynomial (ZMod p)) [Fact (Irreducible h)] : Field (bigF p h) := by
   exact AdjoinRoot.instField
 
 -- Polynomial.factor
@@ -32,6 +32,49 @@ noncomputable instance (p : ℕ ) [Fact (Nat.Prime p)] (h : Polynomial (ZMod p))
 -- polynomial
 
 -- morphism from r to s -- each coefficient of the polynomial is mapped through morphism
+
+section
+  variable {p r : ℕ} [Fact (Nat.Prime p)] {h : Polynomial (ZMod p)} [Fact (Irreducible h)] {h_divides : h ∣ X^r - 1} {A : ℕ}
+
+  noncomputable def f : Polynomial (ZMod p) := X^r - 1
+
+  noncomputable def α : AdjoinRoot (f (p := p) (r := r)) := AdjoinRoot.root f
+
+  noncomputable def H : Submonoid (AdjoinRoot (R := ZMod p) (X^r - 1))
+    := Submonoid.closure
+        {h | ∃ (n : ℕ), n ≤ A ∧ h = AdjoinRoot.of (X^r-1) (↑ n)}
+
+  noncomputable def G : Submonoid (bigF p h) := Submonoid.map (AdjoinRoot.algHomOfDvd h_divides) (H (A := A))
+
+  lemma helper : (aeval ((AdjoinRoot.root (f (p := p) (r := r))) ^ k)) (f (p := p) (r := r)) = 0 := by
+    let f : Polynomial (ZMod p) := X^r - 1
+    let α := AdjoinRoot.root f
+    show (aeval (α ^ k)) f = 0
+    have : (aeval (α ^ k)) f = (α ^ k) ^ r - 1 := by
+      unfold f
+      simp only [map_sub, AdjoinRoot.mk_X, map_pow, aeval_X, map_one]
+
+    rw [this, ← pow_mul, mul_comm k r, pow_mul]
+
+    have : α ^ r = 1 := by
+      have : α ^ r - 1 = 0 := by calc
+        α ^ r - 1 = IsAdjoinRoot.map (AdjoinRoot.isAdjoinRoot f) f := rfl
+        _         = 0 := by simp only [this, AdjoinRoot.isAdjoinRoot_map_eq_mk, AdjoinRoot.mk_self]
+      have : α ^ r - 1 + 1 = 0 + 1 := congrArg (. + 1) this
+      simp only [sub_add_cancel, zero_add] at this
+      assumption
+    simp [this]
+
+  def S : Set ℕ := {
+    k | ∀ g ∈ H (p := p) (r := r) (A := A),
+      g^k = AdjoinRoot.liftHom (f) (α^k) helper g
+    }
+
+  def stmt1 (g : bigF p h) (hg : g ∈ G (p := p) (r := r) (A := A) (h_divides := h_divides)) : g ≠ 0 :=
+    by sorry
+
+
+
 
 example : ℤ →+* (ZMod p) := by exact Int.castRingHom (ZMod p)
 
