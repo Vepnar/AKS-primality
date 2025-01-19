1d6e58e19e06b978b11728b0e14bfb4c7db93303
zuzapio <zpiotrow16@gmail.com>
2025-01-19 19:43:40 +0100

    solved one case, further progress in solving 43

--- LeanTutorial/lemma_43.lean
+++ LeanTutorial/lemma_43.lean
@@ -10,7 +10,32 @@ lemma lemma43 (g q : Polynomial (ZMod p)) (hg : AdjoinRoot.mk (h p r) g ∈ Gmon
   : g = q := by
   let Δ := g - q
   have hΔmod : ∀ k ∈ S n p r, AdjoinRoot.mk (h p r) (Δ.comp X^k) = 0
-  intro X
+  intro w -- changed X to w so it is not confused with X variable, you can always change it back
+  simp
+  intro hX
+  rotate_right 1
+  rw[AdjoinRoot.mk_eq_mk] at hmod
+  rw[dvd_iff_exists_eq_mul_left] at hmod
+  cases' hmod with u uu
+  rw[sub_eq_iff_eq_add] at uu
+  rw[uu]
+  rw[add_left_eq_self]
+  sorry
+
+  --rw[dvd_iff_exists_eq_mul_left]
+  refine And.symm ⟨?_, ?_⟩
+  by_contra hw
+  -- then since w is in S we have that for all functions g in H g 0 = 1 which does not seem very likely, so maybe that might be the rest of the proof
+  sorry
+
+  rw[AdjoinRoot.mk_eq_mk] at hmod
+  exact hmod
+
+
+
+  --refine ext ?_
+  --rintro j
+
 
   --rw [hmod, AdjoinRoot.mk_sub],
   --exact sub_self _, modular equality??
