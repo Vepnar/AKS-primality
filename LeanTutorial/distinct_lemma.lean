6ac777919111b01c7adb378e8f1ecf0dfe5e3ddc
regr4 <60316183+regr4@users.noreply.github.com>
2025-01-26 21:32:57 +0100

    reformulate condition of distinct_lemma

--- LeanTutorial/distinct_lemma.lean
+++ LeanTutorial/distinct_lemma.lean
@@ -3,13 +3,12 @@ import LeanTutorial.basic
 
 variable
   (n p : ℕ) [pprime : Fact (p.Prime)]
-  (hnnotperfpow : ¬ is_perfect_power n) (hn_gt_one : n > 1)
-  (nnotprime : ¬ n.Prime)
+  (hn_not_power_of_p : ∀ k, n ≠ p^k) (hn_gt_one : n > 1)
 
 def m (ij : ℕ × ℕ) : ℕ
   := n ^ ij.1 * p ^ ij.2
 
-include hnnotperfpow hn_gt_one nnotprime in
+include hn_not_power_of_p hn_gt_one in
 lemma distinct : Function.Injective (m n p)
   := by
   -- alternative: use the multiplicity of some prime q ≠ p in n to determine i, and then j is also fixed.
@@ -18,7 +17,7 @@ lemma distinct : Function.Injective (m n p)
   wlog iineq : i₁ ≥ i₂
   . have fact : i₁ ≤ i₂ := by refine Nat.le_of_not_ge iineq
     symm
-    exact this n p hnnotperfpow hn_gt_one nnotprime i₂ j₂ i₁ j₁ eq.symm fact
+    exact this n p hn_not_power_of_p hn_gt_one i₂ j₂ i₁ j₁ eq.symm fact
 
   have nnezero : n ≠ 0 := ne_of_gt (by linarith)
   have pnezero : p ≠ 0 := Nat.Prime.ne_zero pprime.out
@@ -61,18 +60,7 @@ lemma distinct : Function.Injective (m n p)
     _     = p ^ j := rfl
 
   let k := multiplicity p n
-  have n_not_power_of_p : p^k ≠ n := λ f ↦ hnnotperfpow $ by
-    use p, k
-    constructor
-    . exact Nat.Prime.one_lt pprime.out
-    constructor
-    . by_contra hk
-      rw [ge_iff_le, not_le] at hk
-      have : k = 0 ∨ k = 1 := sorry -- ask Alain
-      cases this with
-      | inl kzero => simp[kzero] at f; rw[f] at hn_gt_one; exact Nat.lt_irrefl n $ gt_iff_lt.mp hn_gt_one
-      | inr kone => simp[kone] at f; rw[← f] at nnotprime; exact nnotprime pprime.out
-    . exact f
+  have n_not_power_of_p : p^k ≠ n := λ h ↦ hn_not_power_of_p k h.symm
 
   suffices : i = 0
   . simp only [this, true_and]
--- LeanTutorial/upperBoundG.lean
+++ LeanTutorial/upperBoundG.lean
@@ -6,9 +6,35 @@ variable (n p r : ℕ) (hrnz : r ≠ 0) [pprime : Fact (Nat.Prime p)]
   (childs_binomial_theorem : ∀ a ∈ Finset.range (A n r + 1),
     (α p r + ↑ a)^n = α p r^n + ↑ a)
   (hordern : orderOf (↑ n : ZMod r) > ⌊(Real.logb 2 n) ^ 2 ⌋₊)
+  (hnnotprime : ¬ n.Prime)
 
 def ℓ (ij : ℕ × ℕ) : (ZMod r)ˣ
   := n' n r hrnz hnnoprdivs ^ ij.1 * p' n p r hrnz hp hnnoprdivs ^ ij.2
+
+include hnnotperfpow hn_gt_one hnnotprime in
+lemma n_not_power_of_p : ∀ k : ℕ, p^k ≠ n
+ := λ k f ↦ hnnotperfpow $ by
+    use p, k
+    constructor
+    . exact Nat.Prime.one_lt pprime.out
+    constructor
+    . by_contra hk
+      rw [ge_iff_le, not_le] at hk
+      have : k = 0 ∨ k = 1 := sorry -- ask Alain
+      cases this with
+      | inl kzero => simp[kzero] at f; rw[f] at hn_gt_one; exact Nat.lt_irrefl n $ gt_iff_lt.mp hn_gt_one
+      | inr kone => simp[kone] at f; rw[← f] at hnnotprime; exact hnnotprime pprime.out
+    . exact f
+
+include hnnotperfpow hn_gt_one hnnotprime hp in
+lemma n_over_p_not_power_of_p : ∀ k : ℕ, p^k ≠ n/p
+ := by
+  intro k f
+  replace f := calc
+    p^(k+1) = p^k * p := rfl
+    _ = n/p * p := by rw[f]
+    _ = n := Nat.div_mul_cancel hp
+  exact n_not_power_of_p n p hnnotperfpow hn_gt_one hnnotprime (k+1) f
 
 noncomputable def T : Finset (ℕ × ℕ)
   := let K := Finset.range $ Nat.floor (Real.sqrt $ Nat.card $ R n p r hrnz hp hnnoprdivs) + 1
